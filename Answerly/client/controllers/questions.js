e178da8dca247dd35bc11d1603c7586bf1829e17
Yibing Shi <yibingshi94@gmail.com>
2017-03-01 20:13:13 -0600

    Update questions.js
    Add search

--- Answerly/client/controllers/questions.js
+++ Answerly/client/controllers/questions.js
@@ -16,3 +16,87 @@ myApp.controller('QuestionsController', ['$scope', '$http', '$location', '$route
 		});
 	}
 }]);
+
+var Dict_pro = [{key:"#Prof.F", value:"CHOCOLATE"},{key:"#Prof.T", value:"PSYCHOLOGY"},
+{key:"#Prof.Cobe", value:"STATISTIC"}, {key:"#Prof.A", value:"ENGINEER"},
+{key:"#Prof.Fu", value:"GRAPHIC"}, {key:"#Prof.X", value:"JAVA"},
+{key:"#Prof.L", value:"MBA"}, {key:"#Prof.Q", value:"JAVASCRIPT"},
+{key:"#Prof.S", value:"MANAGEMENT"}];
+var result = new Array();
+
+function select_prof(input){
+    
+    var choice = result[input];
+    // choice = choice.toUpperCase();
+    console.log("choice", choice);
+
+    var change_search = document.getElementById("edit_search").value;
+    
+    if (change_search[0] != "#")
+        document.getElementById("edit_search").value = "";
+
+    document.getElementById("edit_search").value += choice + "    ";
+    result = [];
+    document.getElementById("search_results").innerHTML = "";
+}
+
+function notshowthis(){
+
+    document.getElementById("search_results").innerHTML = "";
+    // document.getElementById("select").style.display = "none";
+}
+
+function getSpecial(input){
+
+    for (var i=0;i<Dict_pro.length;i++){
+            if (Dict_pro[i].key.indexOf(input) != -1){
+
+                console.log("result",Dict_pro[i].value);
+                return Dict_pro[i].value.toLowerCase();
+                
+            }
+    }
+
+}
+
+function find_my_div() {
+
+    //close_all();
+    result = [];
+
+    var o_edit = document.getElementById("edit_search");
+    var str_needle = o_edit.value;
+    
+    
+
+    str_needle = str_needle.toUpperCase();
+
+    if (str_needle == "" || str_needle.length < 3){
+        document.getElementById("search_results").innerHTML = "";
+        result = [];
+    }
+    
+    else{
+        // console.log("str_needle",str_needle);
+
+        for (var i=0;i<Dict_pro.length;i++){
+            if (Dict_pro[i].value.indexOf(str_needle) != -1){
+
+                result.push(Dict_pro[i].key);
+                // console.log("result",result);
+            }
+        }
+    }
+
+    var full_list = "";
+
+    for (var i=0;i<result.length;i++){
+        full_list += '<li onclick="select_prof(' + i + ');\" title=\"'+ getSpecial(result[i]) +'\">' + result[i] + "</li>"; 
+        // console.log("result[i]",result[i]); 
+    }
+
+    // console.log("result",result); 
+    document.getElementById("search_results").innerHTML = full_list;
+
+    //$("#search_results").text(full_list); 
+}
