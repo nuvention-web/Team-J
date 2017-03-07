87782045f5241c7766fc7b7cc06ea3fa9c619e7e
Aagam Shah <aagam.shah1210@gmail.com>
2017-03-07 13:00:30 -0600

    updated email text

--- Answerly/app.js
+++ Answerly/app.js
@@ -34,27 +34,27 @@ app.get('/api/questions', function(req, res){
 });
 
 
-app.post('/api/answers', function(req, res){
-	var ans = req.body;
-	Answer.addQuestion(ans, function(err, ans){
-		if(err){
-			throw err;
-		}
-		res.json(ans);
-	});
-});
+// app.post('/api/answers', function(req, res){
+// 	var ans = req.body;
+// 	Answer.addQuestion(ans, function(err, ans){
+// 		if(err){
+// 			throw err;
+// 		}
+// 		res.json(ans);
+// 	});
+// });
 
-app.get('/api/answers', function(req, res){
-	var ans = req.body;
-	Answer.addAnswers(ans, function(err, ans){
-		if(err){
-			throw err;
-		}
-		res.json(ans);
-	});
-});
+// app.get('/api/answers', function(req, res){
+// 	var ans = req.body;
+// 	Answer.addAnswers(ans, function(err, ans){
+// 		if(err){
+// 			throw err;
+// 		}
+// 		res.json(ans);
+// 	});
+// });
 
-app.post('/api/questions', function(req, res){
+app.post('/api/questions/', function(req, res){
 	var com = req.body;
 	console.log(com);
 	Question.addQuestion(com, function(err, com){
@@ -75,7 +75,7 @@ app.post('/api/questions/email', function(req, res, next){
 	// content = new helper.Content("text/plain", "and easy to do anywhere, even with Node.js");
 	// mail = new helper.Mail(from_email, subject, to_email, content);
 
-	var mail_body = "<h2>"+req.body.question+"</h2><h3><u> Your Answer : <u></h3><h3>"+req.body.answers+"</h3><h3><u> Follow up question : <u></h3><h3>"+req.param('fques')+"</h3>";
+	var mail_body = "<h2>"+req.body.question+"</h2><h3><u> Your Answer : <u></h3><h3>"+req.body.answer+"</h3><h3><u> Follow up question : <u></h3><h3>"+req.param('fques')+"</h3>";
 
 	var sg = require('sendgrid')('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');
 	var request = sg.emptyRequest({
