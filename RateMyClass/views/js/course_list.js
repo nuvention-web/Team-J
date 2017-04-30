c4912cca9bdfab359780ead56b0592a6ae878ea9
Yibing Shi <nathan@dhcp-10-105-85-208.wireless.northwestern.private>
2017-04-30 15:54:12 -0500

    Course list

---
+++ RateMyClass/views/js/course_list.js
@@ -0,0 +1,15 @@
+var app = angular.module('course-list',[]);
+
+app.controller('course-list', function ($scope,$http,$timeout,$window) {	
+
+    $scope.load_course = function(){
+
+        var data = "2016 Fall";
+        $http.get('/courselist').then(function successCallback(response) {
+               $scope.courses = response.data;
+        }, function errorCallback(response){
+                console.log("error");
+        });
+    }
+    
+});
